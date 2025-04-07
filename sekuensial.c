4ec64b427ea8d234ad982d668838818c39acbb3d
Zua <162336746+aisyanajwa@users.noreply.github.com>
2025-04-07 23:32:51 +0700

    Add files via upload

---
+++ sekuensial.c
@@ -0,0 +1,30 @@
+/* sekuensial.c – main */
+
+#include <xinu.h>
+void sndA(void);
+void sndB(void);
+
+process main(void)
+{
+	sndA();
+	sndB();
+	return OK;
+}
+
+void sndA(void)
+{
+	while(1)
+	{
+		printf("A\n");
+		sleepms(1000);	
+	}
+}
+
+void sndB(void)
+{
+	while(1)
+	{
+		printf("B\n");
+		sleepms(1000);	
+	}
+}
